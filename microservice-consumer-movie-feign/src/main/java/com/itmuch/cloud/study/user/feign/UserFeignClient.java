276e08a69d1bfeff90c5c5f4f14d7fb293912f4b
tiger7878 <zhcw63@163.com>
2018-11-08 15:21:31 +0800

    Feign 为服务消费者整合Feign

---
+++ microservice-consumer-movie-feign/src/main/java/com/itmuch/cloud/study/user/feign/UserFeignClient.java
@@ -0,0 +1,23 @@
+package com.itmuch.cloud.study.user.feign;
+
+import com.itmuch.cloud.study.user.entity.User;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Feign接口
+ * User: Administrator
+ * Date: 2018-11-08 14:50
+ */
+@FeignClient(name = "microservice-provider-user") //用于创建Ribbon负载均衡器
+public interface UserFeignClient {
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
+    //注意：在使用Feign时PathVariable必须要传参，否则报错
+//    @GetMapping("/{id}")，使用Fign时，不能用这种简写方式
+    public User findById(@PathVariable("id") Long id);
+
+}
---
+++ microservice-provider-user/src/main/java/com/itmuch/cloud/study/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.itmuch.cloud.study.controller;
+
+import com.itmuch.cloud.study.entity.User;
+import com.itmuch.cloud.study.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * User: monkey
+ * Date: 2018-11-06 11:15
+ */
+@RestController
+public class UserController {
+
+    private static Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    private UserRepository userRepository;
+
+    //GetMapping 是spring4.3提供的注解
+    @GetMapping("/{id}")
+    public User findById(@PathVariable Long id) {
+        User user = userRepository.findOne(id);
+
+        logger.info("provider user findById : " + id);
+
+        return user;
+    }
+}
